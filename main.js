80e53c2de518b2dba2f830d35f8ad4dc187f087c
iiizadora <oliveiras.izadora@gmail.com>
2021-05-31 21:58:03 -0300

    criação da funcionalidade de remover um balão com o evento click

--- main.js
+++ main.js
@@ -1,29 +1,24 @@
-// [FEITO] capturar o elemento html em que vamos inserir os baloes
-// [FEITO] adicionar o balão no container
-// [FEITO] determinar um intervalo de tempo para adicionar os baloes sequencialmente
-
 const containerBaloes = document.querySelector(".container-baloes");
 
 function adicionarBalao() {
   const elementoImg = document.createElement("img");
-
+//adiciona um novo atributo que nesse caso está relacionado com <img src='' class=''>
   elementoImg.setAttribute("src", "./assets/baloon.png");
   elementoImg.setAttribute("class", "balao");
 
-  const valorLeft = Math.round(Math.random() * 90);
+  const valorLeft = Math.round(Math.random() * 90); // para não quebrar a tela
   const valorTop = Math.round(Math.random() * 90);
+  //para gerar números aleatórios e arredondados
 
-  elementoImg.style.left = valorLeft + "vw";
+  elementoImg.style.left = valorLeft + "vw"; // posição referente a tela 
   elementoImg.style.top = valorTop + "vh";
+  //como o .balão está em position: absolute, precisa atribuir um left e top para ele
+  
+  containerBaloes.appendChild(elementoImg); // para adicionar o balão no container criado na section
 
-  console.log("left=>", valorLeft);
-  console.log("Top=>", valorTop);
-
-  containerBaloes.appendChild(elementoImg);
-
-  console.log("Elemento Img =>", elementoImg);
+  elementoImg.addEventListener('click', clicar)
+// função criada para remover a imagem quando for clicada
+  function clicar() {elementoImg.remove(clicar)}
 }
 
-setInterval(adicionarBalao, 3000); //3000 milesegundos = 3 segundos
-
-// adicionarBalao();
+ setInterval(adicionarBalao, 2000); //3000 milesegundos = 3 segundos
